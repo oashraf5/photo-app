a571464da2c1b9d66fc16e06b8a2c29995f7f9d9
oashraf5 <oashraf5@gmail.com>
2020-04-28 01:00:34 -0400

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,15 @@
+const Styles = styled.div`
+  .navbar { background-color: white; }
+ 
+  }
+  .navbar-brand {
+    font-size: 5em;
+    color: black;
+    
+  }
+  .form-center {
+    position: center;
+    left: 25%;
+    right: 25%;
+  }
+`;
